51ab385638b740102b23147b90141eccdf257ae2
Jonas Almeida <jalmeida@mathbiol.org>
2018-11-28 09:52:48 -0500

    caught up with Bill

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
+const fetch = require('node-fetch')
 
 // This is a (sample) collection of books we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
@@ -13,6 +14,22 @@ const books = [
     author: 'Michael Crichton',
   },
 ];
+
+// parms
+
+sparc_url='https://health.data.ny.gov/resource/gnzp-ekau.json?'
+
+// get some SPARCS data
+/*
+fetch('https://health.data.ny.gov/resource/gnzp-ekau.json?$limit=1')
+    .then(res => res.json())
+    .then(json => {
+      lala=json;
+      console.log('done',json)
+      //debugger
+    });
+*/
+
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
@@ -37,7 +54,47 @@ const typeDefs = gql`
   type Query {
     books: [Book]
     date: [Date]
+    records: [Record]
   }
+
+  type Record {
+    abortion_edit_indicator: String
+    age_group: String
+    apr_drg_code: String
+    apr_drg_description: String
+    apr_mdc_code: String
+    apr_mdc_description: String
+    apr_medical_surgical_description: String
+    apr_risk_of_mortality: String
+    apr_severity_of_illness_code: String
+    apr_severity_of_illness_description: String
+    attending_provider_license_number: String
+    birth_weight: String
+    ccs_diagnosis_code: String
+    ccs_diagnosis_description: String
+    ccs_procedure_code: String
+    ccs_procedure_description: String
+    discharge_year: String
+    emergency_department_indicator: String
+    ethnicity: String
+    facility_id: String
+    facility_name: String
+    gender: String
+    health_service_area: String
+    hospital_county: String
+    length_of_stay: String
+    operating_certificate_number: String
+    patient_disposition: String
+    payment_typology_1: String
+    payment_typology_2: String
+    race: String
+    total_charges: String
+    total_costs: String
+    type_of_admission: String
+    zip_code_3_digits: String
+  }
+
+
 `;
 
 // Resolvers define the technique for fetching the types in the
@@ -48,8 +105,13 @@ const resolvers = {
     date: () => [{
       now:Date(),
       hello: "hello at "+Date()
-    }]
-  },
+    }],
+    records: () => {
+      return fetch(sparc_url)
+      .then(response => response.json());
+      //.then(json => console.log(json));
+    }
+  }
 };
 
 // In the most basic sense, the ApolloServer can be started
