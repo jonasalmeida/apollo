68a06db1bed14495905473bd034c6c76d7b1bba9
Jonas Almeida <jalmeida@mathbiol.org>
2018-11-30 08:28:11 -0500

    search box

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ playing with Apollo server and deploying it in heroku
 * live web client: at https://jonasalmeida.github.io/apollo
 * live heroku endpoint: https://i2apollo.herokuapp.com
 * Service registered with Apollo: https://engine.apollographql.com/account/gh.jonasalmeida/services
+* SBU hackathon session: https://sites.google.com/a/mathbiol.org/i2/2018/2018-11-28-nov
--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 console.log('index.js loaded')
 
-var q =`# Write your query or mutation here
-{
-  records(q:"age_group=70 or Older&facility_name=University Hospital")
+funGraphQL = function(){
+    var q =`# Write your query or mutation here
   {
-    facility_name
-    age_group
-    ccs_diagnosis_description
-    ccs_procedure_description
-    length_of_stay
-    total_costs
-  }
-}`
-
-data = JSON.stringify({query:q})
-
-fetch("https://i2apollo.herokuapp.com",
-  {
-    method:'POST',
-    body:data,
-    headers:{
-      "Content-Type": "application/json",
-      //"Accept": "application/json",
-      //"cache-control": "no-cache"
+    records(q:"${queryQraphQL.value}")
+    {
+      facility_name
+      age_group
+      ccs_diagnosis_description
+      ccs_procedure_description
+      length_of_stay
+      total_costs
     }
-  }
-).then(x=>x.json().then(x=>{
-  var div=document.getElementById('graphqlApplication')
-  var pre = document.createElement('pre')
-  div.appendChild(pre)
-  pre.innerHTML=JSON.stringify(x,null,3)
-}))
+  }`
+
+  data = JSON.stringify({query:q})
+
+  fetch("https://i2apollo.herokuapp.com",
+    {
+      method:'POST',
+      body:data,
+      headers:{
+        "Content-Type": "application/json",
+        //"Accept": "application/json",
+        //"cache-control": "no-cache"
+      }
+    }
+  ).then(x=>x.json().then(x=>{
+    var div=document.getElementById('graphqlData')
+    div.innerHTML=''
+    var pre = document.createElement('pre')
+    div.appendChild(pre)
+    pre.innerHTML=JSON.stringify(x,null,3)
+    plotData()
+  }))
+}
+
+funGraphQL()
+
+// Plot Data
+plotData=function(){}
+
+plotButton.onclick=funGraphQL
